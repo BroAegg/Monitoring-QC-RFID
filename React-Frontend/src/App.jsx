68baa0b03566662f8d84b9f898eb3b370a7f6904
Aegner Billik <157568583+BroAegg@users.noreply.github.com>
2025-10-21 20:18:33 +0700

    fix: restore sidebar on MonitoringRFID page

--- React-Frontend/src/App.jsx
+++ React-Frontend/src/App.jsx
@@ -3,36 +3,27 @@
  * Professional RFID scanning and monitoring system
  */
 
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import DaftarRFID from './components/RFID/DaftarRFID';
 import MonitoringRFID from './components/RFID/MonitoringRFID';
 import ListID from './components/RFID/ListID';
 import { RFIDProvider } from './context/RFIDContext';
 
-function AppContent() {
-  const location = useLocation();
-  const isMonitoringPage = location.pathname === '/';
-
-  return (
-    <div className="app">
-      {!isMonitoringPage && <Sidebar />}
-      <main className={isMonitoringPage ? "main-content-fullscreen" : "main-content"}>
-        <Routes>
-          <Route path="/" element={<MonitoringRFID />} />
-          <Route path="/daftar-rfid" element={<DaftarRFID />} />
-          <Route path="/list-id" element={<ListID />} />
-        </Routes>
-      </main>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
       <RFIDProvider>
-        <AppContent />
+        <div className="app">
+          <Sidebar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<MonitoringRFID />} />
+              <Route path="/daftar-rfid" element={<DaftarRFID />} />
+              <Route path="/list-id" element={<ListID />} />
+            </Routes>
+          </main>
+        </div>
       </RFIDProvider>
     </Router>
   );
